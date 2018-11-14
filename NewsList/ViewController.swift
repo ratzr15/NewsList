0bf453dfd4156c8496ede3b2f1d7a70b51fc6411
Rathish Kannan <rathish.k@sunandsandsports.com>
2018-11-14 17:14:34 +0400

    Pass data from list to detail view

--- NewsList/ViewController.swift
+++ NewsList/ViewController.swift
@@ -1,6 +1,6 @@
 //----------------------------------------------------------------------------------
 //  File Name         :  ViewController.swift
-//  Description       :  Controller for Movie Search
+//  Description       :  Controller for List
 //                       1. Manages -> View for list
 //                       2. Invoke -> API to Fetch results
 //  Author            :  Rathish Kannan
@@ -11,15 +11,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate {
     @IBOutlet weak var tableView: UITableView!
    
     fileprivate let viewModel = ListViewModel()
 
     let manager = ListManager()
 
-    var param = Query(country: "us", page: "", id: "6fb52f2be2754dc7871284da64b8e129", count: "")
+    var param = Query(country: "us", page: "", id: "6fb52f2be2754dc7871284da64b8e129", count: "30")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +41,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
 }
 
+//MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: String(describing: DetailsController.self)) as! DetailsController
+        let details = viewModel.items.filter { $0.type == .details }
+        if let detail = details[indexPath.row] as? DetailsItem {
+            vc.viewModel.detail = detail
+        }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
